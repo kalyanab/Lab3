2ef9046f0bb6ef8f01969b427c2466a7dd2299b5
kalyanab <kalyanabhi9955@gmail.com>
2023-09-19 21:36:00 +0530

    Print data types

---
+++ MyPractice4/MyPractice4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MyPractice4
+{
+    using System;
+    class VarDynamic
+    {
+        static void Main()
+        {
+            var i = 100;
+            Console.WriteLine(i.GetType());
+            var c = 'A';
+            Console.WriteLine(c.GetType());
+            var f = 45.67f;
+            Console.WriteLine(f.GetType());
+            var b = true;
+            Console.WriteLine(b.GetType());
+            var s = "Hello";
+            Console.WriteLine(s.GetType());
+           //Console.WriteLine("----------------------------------------------------------------------------------------------------------");
+
+           // dynamic d;
+           // d = 100;
+           // Console.WriteLine(d.GetType());
+           // d = 'Z';
+           // Console.WriteLine(d.GetType());
+           // d = 34.56;
+           // Console.WriteLine(d.GetType());
+           // d = false;
+           // Console.WriteLine(d.GetType());
+           // d = "Hello";
+           // Console.WriteLine(d.GetType());
+        }
+    }
+}
